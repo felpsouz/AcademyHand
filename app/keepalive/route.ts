665f500526d7bba20d6233118c86630a202d5f27
Felipe Souza <129306940+felpsouz@users.noreply.github.com>
2026-04-11 19:47:46 -0300

    update routa do keepalive

--- app/keepalive/route.ts
+++ app/keepalive/route.ts
@@ -1,9 +1,3 @@
-import { NextResponse } from 'next/server';
-
 export async function GET() {
-  return new NextResponse('OK', { status: 200 });
-}
-
-export async function POST() {
-  return new NextResponse('OK', { status: 200 });
+  return new Response('OK', { status: 200 });
 }
