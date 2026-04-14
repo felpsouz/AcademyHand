64aa948813bac953742338e890cf51e7bfeb0e97
Felipe Souza <129306940+felpsouz@users.noreply.github.com>
2026-04-13 22:51:24 -0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: 'academyhand',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+};
